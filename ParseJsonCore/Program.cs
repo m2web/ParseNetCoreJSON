b8907676282cc217aca5bb5e4f1ba131fdac0b7c
m2web <m2web@yahoo.com>
2018-04-27 07:19:12 -0400

    Initial commit

---
+++ ParseJsonCore/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ParseJsonCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = "";
+            var jsonFile = @"C:\Users\m2web\code\ParseJsonCore\ESV_john114.json";
+
+            using (var r = new StreamReader(jsonFile))
+            {
+                json = r.ReadToEnd();
+            }
+
+            var verses = JsonToObject(json);
+
+            foreach (var verse in verses)
+            {
+                Console.WriteLine(verse);
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        public static List<string> JsonToObject(string json)
+        {
+            Passage rootObj = JsonConvert.DeserializeObject<Passage>(json);
+            return rootObj.passages;
+        }
+    }
+
+    public class PassageMeta
+    {
+        public string canonical { get; set; }
+        public List<int> chapter_start { get; set; }
+        public List<int> chapter_end { get; set; }
+        public int prev_verse { get; set; }
+        public int next_verse { get; set; }
+        public List<int> prev_chapter { get; set; }
+        public List<int> next_chapter { get; set; }
+    }
+
+    public class Passage
+    {
+        public string query { get; set; }
+        public string canonical { get; set; }
+        public List<List<int>> parsed { get; set; }
+        public List<PassageMeta> passage_meta { get; set; }
+        public List<string> passages { get; set; }
+    }
+}
---
+++ ParseJsonCore/ProgramOLD.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+namespace ParseJsonCore
+{
+    class ProgramOLD
+    {
+        //static void Main(string[] args)
+        //{
+        //    var json = "";
+        //    var jsonFile = @"C:\Users\m2web\code\ParseJsonCore\ESV_john114.json";
+
+        //    using (var r = new StreamReader(jsonFile))
+        //    {
+        //        json = r.ReadToEnd();
+        //    }
+
+        //    Console.WriteLine(ParseJson(json));
+        //    Console.WriteLine("**************************************");
+        //    Console.WriteLine(ParseToVerse(json));
+        //    Console.WriteLine("**************************************");
+
+        //    Console.WriteLine("Press any key to continue.");
+        //    Console.ReadKey();
+        //}
+
+        static string ParseJson(string inputJson)
+        {
+            var verses = new Verse();
+            try
+            {
+                verses = JsonConvert.DeserializeObject<Verse>(inputJson);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return verses.Passages;
+        }
+
+        public static string ParseToVerse(string json)
+        {
+            var theVerse = new Verse();
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Verse));
+            theVerse = (Verse)ser.ReadObject(ms);
+            ms.Close();
+            return theVerse.Passages;
+        }
+    }
+
+    public class Verse
+    {
+        public string Query;
+        public string Passages;
+    }
+}
