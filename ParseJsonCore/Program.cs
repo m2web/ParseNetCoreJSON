711a8872be1cb14906b74ffda9ec2a7a7fa6ac7a
m2web <m2web@yahoo.com>
2018-04-29 13:01:10 -0400

    Added separare commented main method to access JSON via HttpClient to get to the JSON via a URL.

--- ParseJsonCore/Program.cs
+++ ParseJsonCore/Program.cs
@@ -2,11 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ParseJsonCore
 {
     class Program
     {
+        #region Using JSON File
+
         static void Main(string[] args)
         {
             var json = "";
@@ -40,6 +45,48 @@ namespace ParseJsonCore
             Passage rootObj = JsonConvert.DeserializeObject<Passage>(json);
             return rootObj.passages;
         }
+
+        #endregion
+
+        #region Using HttpClient
+        static HttpClient client = new HttpClient();
+        //static void Main(string[] args)
+        //{
+        //    var verses = RunAsync().GetAwaiter().GetResult();
+        //    foreach (var verse in verses)
+        //    {
+        //        Console.WriteLine(verse);
+        //    }
+        //    Console.WriteLine("Press any key to continue.");
+        //    Console.ReadKey();
+        //}
+
+        static async Task<List<string>> RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("https://api.esv.org/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("/v3/passage/html/?q=john%201%3A1");
+                response.EnsureSuccessStatusCode();
+                var stringResult = await response.Content.ReadAsStringAsync();
+                var rootObj = JsonConvert.DeserializeObject<Passage>(stringResult);
+                return rootObj.passages;
+            }
+            catch (Exception e)
+            {
+                var list = new List<string>();
+                list.Add(e.Message);
+                return list;
+            }
+        }
+
+        #endregion
+
     }
     
 //1. To check if the Json structure is OKAY: http://jsonlint.com/
